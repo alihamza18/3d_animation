4d40bf05834426a51225c97779239a5c227d4e57
Ali Hamza <156931969+alihamza18@users.noreply.github.com>
2024-07-09 19:33:43 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_3d_project/pages/home_page.dart';
+import 'package:flutter_3d_project/pages/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UI 3D flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
